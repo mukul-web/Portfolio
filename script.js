7a61fb40f09ef7e3ea85b6296b5f56ee5c2f37f0
mukul-web <64723465+mukul-web@users.noreply.github.com>
2022-09-25 12:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function sendMail(params) {
-  var tempParams = {
-    from_name: document.getElementById("fromName").value,
-    email_sender: document.getElementById("emailSender").value,
-    subject_sender: document.getElementById("subjectSender").value,
-    message_sender: document.getElementById("message").value,
-  };
-  emailjs.send('{{SERVICE ID}}', '{{TEMPLATE ID}}', tempParams)
-    .then(function(res) {
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Mail Sent Successfully!',
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    });
-}
+
 
 $(document).ready(function() {
   $(window).scroll(function() {
